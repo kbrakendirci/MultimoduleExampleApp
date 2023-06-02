c448441353b4e3f1acc96e71493e0957020aa1cf
kkendirci <kubrakendirci0@gmail.com>
2023-06-02 09:10:13 +0300

    first commit

---
+++ app/src/main/java/com/example/multimoduleexampleapp/MainActivity.kt
@@ -0,0 +1,11 @@
+erpackage com.example.multimoduleexampleapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
